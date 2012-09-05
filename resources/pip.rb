7a7beb7db46e63a7cb9d6c297e6ee6293f0f7038
Saager Mhatre <saager.mhatre@gmail.com>
2012-09-06 01:30:19 +0530

    [COOK-1715] Upgrade pip resource with user & group
    - pass user & group to shellout
    - refactor provider to remove lots of duplication
    - also set HOME to specified user's home to prevent permission errors
      when pip attemts to write ~/.pip/pip.log

--- resources/pip.rb
+++ resources/pip.rb
@@ -22,6 +22,8 @@ actions :install, :upgrade, :remove, :purge
 
 attribute :package_name, :kind_of => String, :name_attribute => true
 attribute :version, :default => nil
-attribute :timeout, :default => nil
+attribute :timeout, :default => 900
 attribute :virtualenv, :kind_of => String
-attribute :options, :kind_of => String
+attribute :user, :regex => Chef::Config[:user_valid_regex]
+attribute :group, :regex => Chef::Config[:group_valid_regex]
+attribute :options, :kind_of => String, :default => ''
