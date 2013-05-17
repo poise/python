71fd738ecc30db24629e34e473818cbb03c93dac
jtimberman <joshua@opscode.com>
2013-05-17 11:43:31 -0600

    [COOK-2866] - use python binary vs existing directory

--- recipes/source.rb
+++ recipes/source.rb
@@ -30,7 +30,7 @@ packages.each do |dev_pkg|
 end
 
 version = node['python']['version']
-install_path = "#{node['python']['prefix_dir']}/lib/python#{version.split(/(^\d+\.\d+)/)[1]}"
+install_path = "#{node['python']['prefix_dir']}/bin/python#{version.split(/(^\d+\.\d+)/)[1]}"
 
 remote_file "#{Chef::Config[:file_cache_path]}/Python-#{version}.tar.bz2" do
   source "#{node['python']['url']}/#{version}/Python-#{version}.tar.bz2"
