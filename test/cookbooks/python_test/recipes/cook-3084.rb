45e88e44fe4517abd1c2516110f4cc705d5287e9
Seth Vargo <sethvargo@gmail.com>
2013-08-28 17:27:32 -0400

    Fix whitespace

--- test/cookbooks/python_test/recipes/cook-3084.rb
+++ test/cookbooks/python_test/recipes/cook-3084.rb
@@ -26,7 +26,7 @@ end
 python_virtualenv "cook-3084-interpreter" do
   # on EL5 the default python we install is called python26
   if !node['python']['install_method'].eql?("source") &&
-     platform_family?('rhel') && 
+     platform_family?('rhel') &&
      node['platform_version'].split('.').first.to_i < 6
     interpreter '/usr/bin/python26'
   else
