691d339bde9bf5dd8968b2d33e4cb5d8318cd142
Akihiro Yamazaki <yamazaki@iij.ad.jp>
2013-12-11 09:48:44 +0900

    use .tgz archive

--- recipes/source.rb
+++ recipes/source.rb
@@ -35,8 +35,8 @@ end
 version = node['python']['version']
 install_path = "#{node['python']['prefix_dir']}/bin/python#{version.split(/(^\d+\.\d+)/)[1]}"
 
-remote_file "#{Chef::Config[:file_cache_path]}/Python-#{version}.tar.bz2" do
-  source "#{node['python']['url']}/#{version}/Python-#{version}.tar.bz2"
+remote_file "#{Chef::Config[:file_cache_path]}/Python-#{version}.tgz" do
+  source "#{node['python']['url']}/#{version}/Python-#{version}.tgz"
   checksum node['python']['checksum']
   mode "0644"
   not_if { ::File.exists?(install_path) }
@@ -45,7 +45,7 @@ end
 bash "build-and-install-python" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOF
-  tar -jxvf Python-#{version}.tar.bz2
+  tar -zxvf Python-#{version}.tgz
   (cd Python-#{version} && ./configure #{configure_options})
   (cd Python-#{version} && make && make #{make_options})
   EOF
