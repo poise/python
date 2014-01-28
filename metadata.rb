18eb0ecbffe854c701d3724ca45af1cdbff5c067
Noah Kantrowitz <noah@coderanger.net>
2014-01-28 00:04:59 -0800

    Bump version for release.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs Python, pip and virtualenv. Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments."
-version           "1.4.5"
+version           "1.4.6"
 
 depends           "build-essential"
 depends           "yum-epel"
