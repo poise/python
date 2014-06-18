ec1dd90b462b1edfacf25a0091f89a099596cbda
Noah Kantrowitz <noah@coderanger.net>
2014-06-17 19:24:13 -0700

    Bump default version
    Closes #106.

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,7 +34,7 @@ end
 default['python']['binary'] = "#{node['python']['prefix_dir']}/bin/python"
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
-default['python']['version'] = '2.7.5'
+default['python']['version'] = '2.7.7'
 default['python']['checksum'] = '3b477554864e616a041ee4d7cef9849751770bc7c39adaf78a94ea145c488059'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
 default['python']['make_options'] = %W{install}
