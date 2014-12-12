a553743ce4d77735229b56aad226703c1ce8fbe8
chantra <chantra@fb.com>
2014-12-12 02:00:41 -0800

    fix python install from source to work with chef 12 Also update the test suite to reflect the right python version

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,8 +35,8 @@ default['python']['binary'] = "#{node['python']['prefix_dir']}/bin/python"
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
 default['python']['version'] = '2.7.7'
-default['python']['checksum'] = '3b477554864e616a041ee4d7cef9849751770bc7c39adaf78a94ea145c488059'
-default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
+default['python']['checksum'] = '7f49c0a6705ad89d925181e27d0aaa025ee4731ce0de64776c722216c3e66c42'
+default['python']['configure_options'] = %W{--prefix=#{node['python']['prefix_dir']}}
 default['python']['make_options'] = %W{install}
 
 default['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
